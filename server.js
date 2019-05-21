84d7beadfc62f31c5458f8e292a5c50de5f48a04
Glitch (hello-express) <none>
2019-05-21 09:12:29 +0000

    🍭🐂 Checkpoint ./server.js:4412243/125 ./messages.json:4412243/167

--- server.js
+++ server.js
@@ -7,29 +7,29 @@ const texts = require("./messages.json");
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
-const welcomeMessage = {
-  "id":0,
-  "from": "Bart",
-  "text": "Welcome to CYF chat system!"
-}
+// const welcomeMessage = {
+//   "id":0,
+//   "from": "Bart",
+//   "text": "Welcome to CYF chat system!"
+// }
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
- const messages = [welcomeMessage]
+ // const messages = [welcomeMessage]
 
-app.post('/messages', function(request, response) {
-  const message =request.body
-  console.log(message)
-  message.id =messages.length+1
-  messages.push(message)
-  response.status(201).json(message) 
+// app.post('/messages', function(request, response) {
+//   const message =request.body
+//   console.log(message)
+//   message.id =messages.length+1
+//   messages.push(message)
+//   response.status(201).json(message) 
   
   app.get("/chats", function(request, response){
   response.json(messages);
 });
   
-});
+// });
 app.listen(process.env.PORT);
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
