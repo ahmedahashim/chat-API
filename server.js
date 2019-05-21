5f49a8ef27ae53c01bdd0b776a3bd91c6b14c534
Glitch (hello-express) <none>
2019-05-21 00:05:23 +0000

    🏠👚 Checkpoint ./server.js:4412243/340

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require('cors')
-
+const chats = require("./chats.json");
 const app = express();
 
 app.use(cors())
@@ -15,7 +15,12 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage]
-
+app.post('/chats', function(request, response) {
+  const chat =request.body
+  chat.id =chats.length+1
+  chats.push(chat)
+  response.status(201).json(chat)
+});
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
