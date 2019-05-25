bb54a328664540835311b317e0b731caa17e4013
Glitch (hello-express) <none>
2019-05-25 02:30:17 +0000

    🗾🎟 Checkpoint ./server.js:4412243/360

--- server.js
+++ server.js
@@ -19,17 +19,23 @@ const welcomeMessage = {
  const messages = [welcomeMessage]
  
 //Create a new message
- app.post('/messages', function(request, response,err) {
-  
-  const message ={}
-  message.create(request.body).then(function(message){
+ app.post('/messages', function(request, response) {
+  const message =request.body
+ 
   message.id =messages.length+1
   messages.push(message)
-  response.status(201).json(message)}
-  
+  response.status(201).json(message)
+   
    });
- 
-
+   
+app.post('/messages', function(request, response) {
+  
+  const message =request.body
+  message.text!= ""
+  response.sendStatus(400)
+   
+   
+   });
 
 
 
